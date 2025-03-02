4f09349affb59fa8c32a061025334b7d32556f8d
Bitcoin <umidasirov55@gmail.com>
2025-03-02 22:02:16 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.scss'
 import { colors } from './constant/colors'
 import Header from './tanalar/Header'
 import { Stack } from '@mui/material'
+import Navbar from './tanalar/navbar'
 function App() {
 
   return (
-    <Stack sx={{background:colors.secondary,height:"100vh"}}>
-    <Header />
+    <Stack sx={{background:colors.secondary,height:"100vh"}} className='App'>
+      <Header />
+      <Navbar /> 
     </Stack>
   )
 }
---
+++ src/tanalar/navbar.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar(props) {
+  return (
+    <div className="navbar">
+      <nav>
+        <NavLink to="/"><i class="fa-solid fa-house"></i> <span>Main</span></NavLink>
+        <NavLink to="/music"><i class="fa-solid fa-music"></i><span>Music</span></NavLink>
+        <NavLink to="/sport"><i class="fa-solid fa-volleyball"></i><span>Sport</span></NavLink>
+        <NavLink to="/news"><i class="fa-solid fa-newspaper"></i><span>News</span></NavLink>
+      </nav>
+    </div>
+  )
+}
