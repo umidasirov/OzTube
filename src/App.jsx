b1f895d8d258ae368f7761f66c71b2c4f2db00ac
umid <umidasirov55@gmail.com>
2025-03-04 12:55:18 +0500

    Ваше сообщение коммита

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.scss'
 import { colors } from './constant/colors'
 import Header from './tanalar/Header'
+import Navbar from './tanalar/Navbar'
 import { Stack } from '@mui/material'
-import Navbar from './tanalar/navbar'
-function App() {
+import { Route, Routes } from 'react-router-dom'
 
+function App() {
   return (
-    <Stack sx={{background:colors.secondary,height:"100vh"}} className='App'>
+    <Stack sx={{ background: colors.secondary }} className='App'>
       <Header />
-      <Navbar /> 
+      <Navbar />
+        <div className="content">
+          <Routes>
+            {/* Указываем правильный компонент, который должен отобразиться на главной странице */}
+            <Route path='/' element={<div>Главная страница</div>} />
+            {/* Пример добавления других маршрутов */}
+            {/* <Route path='/about' element={<About />} /> */}
+          </Routes>
+        </div>
     </Stack>
   )
 }
